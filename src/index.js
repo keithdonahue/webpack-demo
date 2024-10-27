eb703f0806b54b49906913aca6debf7188d2778a
Keith Donahue <bkshankz@gmail.com>
2024-10-27 18:34:39 -0400

    output management is complete

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 
 import _ from 'lodash';
-import './style.css';
-import imageSrc from './image1.png';
-import data from './data.xml';
-import notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml,':toml');
-console.log(yaml,':yaml');
-console.log(json,':json');
-
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
-  // Lodash, is now improted by the script
+  const btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello','webpack'], ' ');
-  element.classList.add('hello');
 
-  // add the image to our html
-  const image1 = new Image();
-  image1.src = imageSrc;
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
 
-  element.appendChild(image1);
-  console.log(data, ':data');
-  console.log(notes, ':notes');
+  element.appendChild(btn);
 
   return element;
 }
